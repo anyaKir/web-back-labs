4ed5c389e6239ccf0c7687f175be3c28a33c1133
Кирдячкина Анна <kirdachkina2004@gmail.com>
2025-09-24 16:34:32 +0700

    Правки

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def not_found(err):
     <html lang="ru">
         <head>
             <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>404</title>
             <style>
                 body {{
