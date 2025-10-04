0e7d067b2c53fc549a3f84a1246e6ae7cc39cf2e
Кирдячкина Анна <kirdachkina2004@gmail.com>
2025-10-04 21:11:22 +0700

    Имя студента

--- app.py
+++ app.py
@@ -341,5 +341,6 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Анна Кирдячкина'
+    return render_template('example.html', name=name)
     
