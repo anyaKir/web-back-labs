d1424d5a5b0d7b8cea0cf91b35be0a6ca6d6b9e9
Кирдячкина Анна <kirdachkina2004@gmail.com>
2025-10-04 21:04:36 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect, render_template, abort
+from flask import Flask, url_for, request, redirect, render_template, abort, render_template
 import datetime
 app = Flask(__name__)
 
@@ -338,4 +338,8 @@ def add_flower(name):
     </body>
 </html>
 '''
+
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
     
