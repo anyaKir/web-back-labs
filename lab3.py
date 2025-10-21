18539e093450a2903965c7c43bb1016ad8f8246e
Кирдячкина Анна <kirdachkina2004@gmail.com>
2025-10-21 19:16:30 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -8,6 +8,7 @@ def lab():
     name_color = request.cookies.get('name_color')
     return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
+
 @lab3.route('/lab3/cookie')
 def cookie():
     resp = make_response(redirect('/lab3/'))
@@ -16,6 +17,7 @@ def cookie():
     resp.set_cookie('name_color', 'magenta')
     return resp
 
+
 @lab3.route('/lab3/del_cookie')
 def del_cookie():
     resp = make_response(redirect('/lab3/'))
@@ -23,3 +25,11 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp 
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age =  request.args.get('age')
+    sex =  request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+
