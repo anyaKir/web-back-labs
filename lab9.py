290e028a6b8927c516cf2e80bda0571a7cfbf45c
Анна Кирдячкина <kirdachkina2004@gmail.com>
2025-12-16 20:58:07 +0700

    правки

--- lab9.py
+++ lab9.py
@@ -44,7 +44,7 @@ def db_connect():
     # SQLite как запасной вариант
     try:
         dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "..", "database.db")
+        db_path = path.join(dir_path, "database.db")
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
